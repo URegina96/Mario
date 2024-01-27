c8e0b12a00dad30a0dce320a95c93e1951286a5c
regina <usmanova.re@yandex.ru>
2024-01-27 22:58:39 +0500

    Для набора спрайтов  индексы их на исходном изображении закономерны. Например, спрайты кустов -  это три клетки по горизонтали. Альтернативные версии кустов просто начинаются с другой позиции  Поэтому мы можем создать вспомогательные методы для генерации наборов по начальной позиции. Добавьте в  Sprite функции по созданию  спрайтов  в companion

--- src/jsMain/kotlin/Sprites.kt
+++ src/jsMain/kotlin/Sprites.kt
@@ -1,3 +1,12 @@
-data class Sprite(var src: String, val si: Int, val sj: Int, val w: Int = 1, val h: Int = 1)
 const val TILES_IMAGE = "sprites/tiles.png"
 val cloudSprite = Sprite(TILES_IMAGE, si = 0, sj = 20, w = 3, h = 2)
+data class Sprite(var src: String, val si: Int, val sj: Int, val w: Int = 1, val h: Int = 1) {
+    companion object {
+
+        fun tile(si: Int, sj: Int, w: Int = 1, h: Int = 1) = Sprite(TILES_IMAGE, si, sj, w, h)
+
+        fun bush(si: Int, sj: Int) = List(3) {
+            tile(si + it, sj)
+        }
+    }
+}
