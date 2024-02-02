dd6c7587264a4ab92f85c70dcd700b78953aa2ef
regina <usmanova.re@yandex.ru>
2024-02-02 23:24:44 +0500

    Класс Hero для главного действующего лица  Дам Марио возможность двигаться При этом пусть он  смотрит в  сторону направления движения   При смене направления  нужно менять спрайт, а точнее исходное изображение в спрайте  Для этого сделаю свойство src изменяемым

--- src/jsMain/kotlin/Entity.kt
+++ src/jsMain/kotlin/Entity.kt
@@ -1,5 +1,5 @@
 //Entity - объект, сущность. Объекты в игре характеризуются расположением  и изображением (спрайтом)
 
-class Entity(val x: Double, val y: Double, val sprite: Sprite) {
+open class Entity(var x: Double, var y: Double, var sprite: Sprite) {
     constructor(i: Int, j: Int, sprite: Sprite) : this(i.toDouble(), j.toDouble(), sprite)
 }
