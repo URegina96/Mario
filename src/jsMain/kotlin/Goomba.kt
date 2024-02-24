f36b7e68b34ca1b971a9bade80e88a116df62185
regina <usmanova.re@yandex.ru>
2024-02-24 22:30:48 +0500

    class Goomba
    Добавила функции движения goomba

--- src/jsMain/kotlin/Goomba.kt
+++ src/jsMain/kotlin/Goomba.kt
@@ -4,10 +4,104 @@ class Goomba(i: Int = 0, j: Int = 0, sprites: List<Sprite> = listOf(), val onDis
 Внутри колбэка this будет этой гумбой
 свойство isDisappearing для таких ситуаций, когда объект еще есть, но уже не реагирует ни на что
  */
+    private var isWalking = false
+    private var isFacingForward = true
+    var isJumping = false
+
     override fun onTopSideCollisionWith(that: Entity) {
         super.onTopSideCollisionWith(that)
         if (that is Hero) {
             isDisappearing = true
+            onDisappear()
         }
     }
+
+    fun moveRight() {
+        isWalking = true
+        isFacingForward = true
+        isJumping = false
+        sprite.src = GOOMBA_IMAGE
+        x += 0.3
     }
+
+    fun moveLeft() {
+        isWalking = true
+        isFacingForward = false
+        isJumping = false
+        sprite.src = GOOMBA_IMAGE
+        x -= 0.3
+    }
+
+    override fun update(dt: Double) {
+        // Добавить здесь логику обновления состояния гумбы
+        if (isWalking) {
+            if (isFacingForward) {
+                moveRight()
+            } else {
+                moveLeft()
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//private var isWalking = false
+//    private var isFacingForward = true
+//    var isJumping = false
+//
+//    override fun update(dt: Double) {
+//        super.update(dt)
+//
+//        if (!isWalking) {
+//            moveRight()
+//        }
+//
+//        if (isColliding()) {
+//            isFacingForward = !isFacingForward
+//            if (isFacingForward) {
+//                moveRight()
+//            } else {
+//                moveLeft()
+//            }
+//        }
+//    }
+//
+//    private fun isColliding(): Boolean {
+//        // Логика проверки столкновений с преградами или другими гумбами
+//        return false
+//    }
+//
+//    override fun onTopSideCollisionWith(that: Entity) {
+//        super.onTopSideCollisionWith(that)
+//        if (that is Hero) {
+//            isDisappearing = true
+//            onDisappear()
+//        }
+//    }
+//
+//    fun moveRight() {
+//        isWalking = true
+//        isFacingForward = true
+//        isJumping = false
+//        sprite.src = GOOMBA_IMAGE
+//        x += 0.3
+//    }
+//
+//    fun moveLeft() {
+//        isWalking = true
+//        isFacingForward = false
+//        isJumping = false
+//        sprite.src = GOOMBA_IMAGE
+//        x -= 0.3
+//    }
+//}
